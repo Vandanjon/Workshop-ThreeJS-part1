170f30e3fe4c1d2aec17f9716c35256743a919b4
Benoît V <benoit@vandanjon.fr>
2022-10-04 15:02:03 +0200

    step 4

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,3 +8,16 @@ scene.background = new THREE.TextureLoader().load("./background_space.jpg");
 // ADD CAMERA
 const camera = new THREE.PerspectiveCamera(75, wpWidth / wpHeight, 1, 1000);
 camera.position.z = 5;
+
+// ADD RENDER
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(wpWidth, wpHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// Function Animate
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+animate();
