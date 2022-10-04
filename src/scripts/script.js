24e74bb392f998054aef626cbfc01090838ece69
Benoît V <benoit@vandanjon.fr>
2022-10-04 15:00:34 +0200

    step 3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,10 @@
 const wpWidth = window.innerWidth;
 const wpHeight = window.innerHeight;
 
+// ADD SCENE
 const scene = new THREE.Scene();
 scene.background = new THREE.TextureLoader().load("./background_space.jpg");
+
+// ADD CAMERA
+const camera = new THREE.PerspectiveCamera(75, wpWidth / wpHeight, 1, 1000);
+camera.position.z = 5;
