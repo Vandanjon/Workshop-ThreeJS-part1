bb5564aba88e537d83140bad8584729f90bcb71e
Benoît V <benoit@vandanjon.fr>
2022-10-04 15:10:47 +0200

    step 5

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,7 @@ const wpHeight = window.innerHeight;
 
 // ADD SCENE
 const scene = new THREE.Scene();
-scene.background = new THREE.TextureLoader().load("./background_space.jpg");
+scene.background = new THREE.TextureLoader().load("./assets/background_space.jpg");
 
 // ADD CAMERA
 const camera = new THREE.PerspectiveCamera(75, wpWidth / wpHeight, 1, 1000);
@@ -13,6 +13,13 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(wpWidth, wpHeight);
 document.body.appendChild(renderer.domElement);
+
+// ADD PLANET EARTH
+const textureEarth = new THREE.TextureLoader().load("./assets/texture_earth.jpg");
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const material = new THREE.MeshPhongMaterial({ map: textureEarth });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 
 // Function Animate
