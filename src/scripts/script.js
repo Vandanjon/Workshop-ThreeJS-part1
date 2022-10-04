977380567c341da596af7902e3ef24df22cc15ef
Benoît V <benoit@vandanjon.fr>
2022-10-04 14:58:25 +0200

    step 2

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,5 @@
-console.log("script.js linked to index.html");
+const wpWidth = window.innerWidth;
+const wpHeight = window.innerHeight;
+
+const scene = new THREE.Scene();
+scene.background = new THREE.TextureLoader().load("./background_space.jpg");
